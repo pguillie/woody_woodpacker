457c5fa59a87c96ba1a23f51d295b1ca8e9c20b9
pguillie <pguillie@student.42.fr>
2019-04-12 14:46:08 +0200

    print ``WOODY''

--- sources/opcode.s
+++ sources/opcode.s
@@ -1,17 +1,29 @@
 foo:
+	push	rax
 	push	rdi
-	push	rcx
+	push	rsi
 	push	rdx
 
-	lea	rdi, [rel bar]
-	xor	rcx, rcx
+	lea	rdi, [rel foo]
+	xor	rax, rax
 bar:
-	xor	byte [rdi + rcx], 0x42
-	inc	rcx
-	cmp	rcx, 0x1000
+	xor	byte [rdi + rax], 0x42
+	inc	rax
+	cmp	rax, 0x42424242
 	jl	bar
 
+	call	baz
+	db	"....WOODY....", 0xa
+
+baz:
+	mov	rax, 0x1
+	mov	rdi, 0x1
+	pop	rsi
+	mov	rdx, 0xe
+	syscall
+
 	pop	rdx
-	pop	rcx
+	pop	rsi
 	pop	rdi
+	pop	rax
 	jmp	0x42424246
